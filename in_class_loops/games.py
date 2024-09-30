889be31eaaffb1ca779865803ec70163aa7edc99
mal compaore <mcompaore@unc.edu>
2024-09-30 19:36:07 -0400

    assignment_03

---
+++ in_class_loops/games.py
@@ -0,0 +1,12 @@
+games = ['The Sims 3', 'Dead by Daylight', 'Minecraft', 'Horizon: Zero Dawn']
+
+i = 0
+
+for game in games:
+    i += 1
+print(f"There are {i} games in this list.")
+
+
+print(f"These are my favorite games: ")
+for game in games:
+    print(game)
