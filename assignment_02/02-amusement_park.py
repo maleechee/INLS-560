c9668eb159c38d94155939ecc2234e7e91cefb27
Maliah Compaore <mcompaore@unc.edu>
2024-10-07 17:50:26 -0400

    Updated comments for 02-amusement_park.py

--- assignment_02/02-amusement_park.py
+++ assignment_02/02-amusement_park.py
@@ -2,12 +2,14 @@
 
 age = int(input(f"Enter your age: "))
 
-# The 'age >= 100' must be before 'age > 60' because order matters.
+# Note: The 'age >= 100' must be before 'age > 60' because order matters.
 if age < 4 or age >= 100:
     print("Admission is free!")
 elif age < 18: 
     print("Admission is $25.")
 elif age > 60:
     print("Admission is $35.")
+# elif age >= 100:
+#   print("Admission is free!")
 else: 
     print("Admission is $40.")
