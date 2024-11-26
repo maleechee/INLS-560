4d2cfcca0e441cafc3aae780f0cc60c5cf39d15f
mal compaore <mcompaore@unc.edu>
2024-11-25 21:40:28 -0500

    0 header

--- assignment_07/roster.py
+++ assignment_07/roster.py
@@ -1,7 +1,11 @@
 # Roster Webpage Link: https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
 
 roster = ['Brown', 'Davis', 'Tyson']
 print(roster)
 
 for name in roster:
     print(name)
+
+data = pd.DataFrame[roster]
+print(data)
